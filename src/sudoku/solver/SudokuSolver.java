b8b91b36d10f58178ad2476ec64f1556758936a9
vaivanov <zlumyo@gmail.com>
2014-05-10 12:41:43 +0400

    Base GUI has been designed.

--- src/sudoku/solver/SudokuSolver.java
+++ src/sudoku/solver/SudokuSolver.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package sudoku.solver;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
+import org.controlsfx.dialog.Dialogs;
 
 /**
- *
+ * Main application class.
  * @author Владимир
  */
 public class SudokuSolver extends Application {
@@ -23,22 +23,57 @@ public class SudokuSolver extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Solve sudoku");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                Dialogs msgBox = Dialogs.create();
+                msgBox.title("Message");
+                msgBox.message("Not implemented yet!");
+                msgBox.masthead("Error");
+                msgBox.showInformation();
             }
         });
+ 
+        GridPane sudokuTable = new GridPane();
+        sudokuTable.setAlignment(Pos.CENTER);
+        sudokuTable.setHgap(5);
+        sudokuTable.setVgap(5);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                ComboBox tempBox = new ComboBox();
+                tempBox.getItems().addAll(
+                    "0",
+                    "1",
+                    "2",
+                    "3",
+                    "4",
+                    "5",
+                    "6",
+                    "7",
+                    "8",
+                    "9"
+                );
+                tempBox.setValue("0");
+                sudokuTable.add(tempBox, i, j);
+            }
+        }
         
-        Scene scene = new Scene(root, 300, 250);
+        VBox vbox = new VBox(sudokuTable, btn);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(10));
+        vbox.setSpacing(10);
         
-        primaryStage.setTitle("Hello World!");
+        StackPane root = new StackPane(vbox);
+        root.setAlignment(Pos.CENTER);     
+        
+        Scene scene = new Scene(root, 260, 250);
+        
+        primaryStage.setTitle("Sudoku solver");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
