2880316db0052891a19905d81041f6daa73507c9
vaivanov <zlumyo@gmail.com>
2014-05-29 21:39:27 +0400

    Usage of JPL was "truely" realized.

--- src/sudoku/solver/SudokuModel.java
+++ src/sudoku/solver/SudokuModel.java
@@ -1,6 +1,7 @@
 package sudoku.solver;
 
 import jpl.*;
+import java.util.*;
 
 /**
  * Realizes interaction with SWI-Prolog.
@@ -18,27 +19,54 @@ public class SudokuModel {
     }
     
     public int[][] solve(int[][] cells) {
-        Query q = new Query("sudoku(" + buildMatrix(cells) + ", Result)");      
-        return (int[][])q.getSolution().get("Result");
+        Query q = new Query("sudoku(" + buildMatrix(cells) + ", Result)");
+        
+        Term[] rows = Util.listToTermArray((Term)q.oneSolution().get("Result"));
+        int[][][] matrix = new int[rows.length][rows.length][];
+        for (int i = 0; i < rows.length; ++i) {
+            Term[] temp = Util.listToTermArray(rows[i]);
+            for (int j = 0; j < temp.length; ++j) {
+                Term[] tempTerm = Util.listToTermArray(temp[j]);
+                int[] temp2 = new int[tempTerm.length];
+                
+                for (int k = 0; k < tempTerm.length; ++k) {
+                    temp2[k] = tempTerm[k].intValue();
+                }
+                        
+                matrix[i][j] = temp2;
+            }
+        }
+        
+        return new int[][] {};
     }
     
     private String buildMatrix(int[][] cells) {
-        String result = "[";
+        StringBuilder result = new StringBuilder("[");
         
+        List<String> strList = new ArrayList<>();
         for (int[] i : cells) {
-            result += buildList(i) + ",";
+            strList.add(buildList(i));
         }
         
-        return result + "]";
+        result.append(String.join(",", strList));
+        
+        result.append("]");
+        
+        return result.toString();
     }
     
     private String buildList(int[] line) {
-        String result = "[";
+        StringBuilder result = new StringBuilder("[");
         
+        List<String> intList = new ArrayList<>();
         for (int i : line) {
-            result += i + ",";
-        }
+            intList.add(String.valueOf(i));
+        }     
+  
+        result.append(String.join(",", intList));
         
-        return result + "]";
+        result.append("]");
+        
+        return result.toString();
     } 
 }
